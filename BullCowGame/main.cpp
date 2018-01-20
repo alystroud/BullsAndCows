c2b60bd7c2bd6fc5a03c136dfe7fe0f91742c6d3
alystroud <alygrace@hotmail.co.uk>
2018-01-20 18:57:55 +0000

    Initial Commit

---
+++ BullCowGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BullCowGame
+//
+//  Created by Aly Stroud on 20/01/2018.
+//  Copyright © 2018 Aly Stroud. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
